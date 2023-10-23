1d1dfa830f69957270fcd2cea57fe5aeff5f6710
rao <138585813+Raomine@users.noreply.github.com>
2023-10-23 12:08:20 +0000

    Html et Css de la page de logement

--- src/Logements.js
+++ src/Logements.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import Logox from "./assets/LOGOKasax1.5.svg";
 import LogoFooter from "./assets/LOGOFooter.png";
+import Cover from "./assets/Background_img.png";
+import { FaStar } from "react-icons/fa6";
+import { FaAngleUp } from "react-icons/fa6";
+import React, { useState } from "react";
 
 export default function Logements() {
   return (
@@ -25,27 +29,113 @@ function Header() {
 }
 
 function Logement() {
+  const [open, setOpen] = useState(false);
+  const [openTwo, setOpenTwo] = useState(false);
   return (
     <>
       <div className="carrousel">
-        <img src="" alt=""></img>
+        <img src={Cover} alt="Photo du logement"></img>
       </div>
-      <div className="content">
-        <p className="title">
-          Cozy loft on the Canal Saint-Martin <span>Paris, Île-de-France</span>
-        </p>
-        <div className="host">
-          <p className="name">Alexandre Dumas</p>
-          <div className="picture"></div>
+      <div className="contents">
+        <div className="content">
+          <p className="title">
+            Cozy loft on the Canal Saint-Martin{" "}
+            <span>Paris, Île-de-France</span>
+          </p>
+          <div className="host">
+            <p className="name">Alexandre Dumas</p>
+            <div className="picture"></div>
+          </div>
         </div>
-      </div>
-      <div className="infos">
-        <div className="tags">
-          <p className="tag">Cozy</p>
-          <p className="tag">Canal</p>
-          <p className="tag">Paris 10</p>
+        <div className="infos">
+          <div className="tags">
+            <p className="tag">Cozy</p>
+            <p className="tag">Canal</p>
+            <p className="tag">Paris 10</p>
+          </div>
+          <div className="rating">
+            <FaStar
+              style={{
+                color: "#ff6060",
+                fontSize: "32px",
+                paddingRight: "10px",
+              }}
+            />
+            <FaStar
+              style={{
+                color: "#ff6060",
+                fontSize: "32px",
+                paddingRight: "10px",
+              }}
+            />
+            <FaStar
+              style={{
+                color: "#ff6060",
+                fontSize: "32px",
+                paddingRight: "10px",
+              }}
+            />
+            <FaStar
+              style={{
+                color: "#E3E3E3",
+                fontSize: "32px",
+                paddingRight: "10px",
+              }}
+            />
+            <FaStar style={{ color: "#E3E3E3", fontSize: "32px" }} />
+          </div>
         </div>
-        <div className="rating"></div>
+        <section>
+          <div>
+            <div className="descriptions">
+              <p className="description">Description</p>
+              <FaAngleUp
+                className={`arrow ${open ? `active` : `inactive`}`}
+                style={{
+                  color: "white",
+                  fontSize: "32px",
+                  alignSelf: "center",
+                }}
+                onClick={() => {
+                  setOpen(!open);
+                }}
+              />
+            </div>
+            <p className={`logementText ${open ? `active` : `inactive`}`}>
+              Profitez du charme de la vie parisienne dans un magnifique
+              appartement. A 3 minutes à pied du Canl Saint Martin, vous serez
+              proche des transports, mais également de nombreux commerces.
+              L'appartement est tout équipé, et possède également un parking
+              pour ceux qui souhaitent se déplacer en voiture.
+            </p>
+          </div>
+          <div>
+            <div className="equipements">
+              <p className="equipement">Équipements</p>
+              <FaAngleUp
+                className={`arrow ${openTwo ? `active` : `inactive`}`}
+                style={{
+                  color: "white",
+                  fontSize: "32px",
+                  alignSelf: "center",
+                }}
+                onClick={() => {
+                  setOpenTwo(!openTwo);
+                }}
+              />
+            </div>
+            <ul
+              className={`logementTextTwo ${openTwo ? `active` : `inactive`}`}
+            >
+              <li>Parking</li>
+              <li>Sèche Cheveux</li>
+              <li>Machine à laver</li>
+              <li>Wi-fi</li>
+              <li>Cuisine équipée</li>
+              <li>Télévision</li>
+            </ul>
+          </div>
+        </section>
       </div>
     </>
   );
