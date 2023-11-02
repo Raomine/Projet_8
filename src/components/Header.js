8210fb8699ab7c8d4c050144fde6119b41f668ae
rao <138585813+Raomine@users.noreply.github.com>
2023-11-02 19:39:48 +0000

    Début du media querie

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <img src={Logox} alt="Logo Kasa"></img>
+      <div className="logo"></div>
       <nav>
         <Link to="/">Accueil</Link>
         <Link to="/About">A Propos</Link>
